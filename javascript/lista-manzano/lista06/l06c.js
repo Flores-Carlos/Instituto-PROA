bfaf77f1ae46105d580c0c4bb4ebc83aaf6d26b2
Carlos Flores <carlos.freitas15@outlook.com>
2022-04-23 20:46:15 -0300

    Update l06c.js

--- javascript/lista-manzano/lista06/l06c.js
+++ javascript/lista-manzano/lista06/l06c.js
@@ -6,5 +6,5 @@ for (let i = 1; i <= 20; i++) {
     a[i] = Number(prompt("Valor: "))
     b[i] = Number(prompt("Menos: "))
     c[i] = a[i] - b[i]
-    alert("Igual a " + c[i])
+    alert(`${a[i]} - ${b[i]} = ${c[i]}`)
 }
