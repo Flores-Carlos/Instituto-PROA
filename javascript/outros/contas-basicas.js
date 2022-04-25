7539fd4ab42bdaa1c454e989051d392fbb9e1519
Carlos Flores <carlos.freitas15@outlook.com>
2022-04-25 19:56:15 -0300

    Realocando alguns arquivos

--- javascript/outros/contas-basicas.js
+++
@@ -1,9 +0,0 @@
-let n1, n2;
-
-n1 = parseInt(prompt("Digite um número: "));
-n2 = parseInt(prompt("Digite outro número: "));
-
-alert("Soma: " + (n1 + n2));
-alert("Subtração: " + (n1 - n2));
-alert("Multiplicação: " + (n1 * n2));
-alert("Divisão: " + (n1 / n2));
