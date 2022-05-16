0d8e0719cc95de3f7da7801749062d9b87fbb891
Carlos Flores <carlos.freitas15@outlook.com>
2022-05-16 20:14:23 -0300

    Projeto ToDo List Incompleto

---
+++ reactjs/projeto-todo-list/src/App.jsx
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+import AddTask from './components/AddTask';
+import Task from './components/Task';
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      tasks: [],
+    };
+
+    this.createTask = this.createTask.bind(this);
+    this.removeTask = this.removeTask.bind(this);
+    this.updateTask = this.updateTask.bind(this);
+  }
+
+  createTask(newTask) {
+    const { tasks } = this.state;
+    this.setState({
+      tasks: [...tasks, newTask],
+    });
+  }
+
+  updateTask(updatedTask) {
+    const { tasks } = this.state;
+    const updatedTasks = tasks.map((task) => {
+      const taskToUpdate = task;
+      if (taskToUpdate.id === updatedTask.id) {
+        taskToUpdate.hasFinished = updatedTask.hasFinished;
+      }
+      return taskToUpdate;
+    });
+    this.setState({
+      tasks: updatedTasks,
+    });
+  }
+
+  removeTask(id) {
+    const { tasks } = this.state;
+    const updatedTasks = tasks.filter((task) => task.id !== id);
+    this.setState({
+      tasks: updatedTasks,
+    });
+  }
+
+  render() {
+    const { tasks } = this.state;
+    return (
+      <>
+        <AddTask onCreate={this.createTask} />
+        {tasks.map((task) => (
+          <Task
+            key={task.id}
+            data={task}
+            onUpdate={this.updateTask}
+            onRemove={this.removeTask}
+          />
+        ))}
+      </>
+    );
+  }
+}
+
+export default App;
---
+++ reactjs/projeto-todo-list/src/components/Task.jsx
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export default class Task extends Component {
+    constructor(props) {
+        super(props);
+
+        const { data } = this.props;
+        this.state = {
+            id: this.data.id,
+            hasFinished: false,
+        };
+
+        this.handleCheckbox = this.handleCheckbox.bind(this);
+    }
+
+    handleCheckbox(event) {
+        const { onUpdate } = this.props;
+        const currentState = this.state;
+        this.setState({
+            hasFinished: event.target.checked,
+        });
+
+        onUpdate({
+            ...currentState,
+            hasFinished: event.target.checked,
+        });
+    }
+
+    render() {
+        const { data, onRemove } = this.props;
+        const { hasFinished } = this.state;
+        const { id, title } = data;
+        return (
+            <div>
+                <input type="checkbox" onChange={this.handleCheckbox} checked={hasFinished} />
+                {title}
+                <button type='button' onClick={() => onRemove(id)}>Remover</button>
+            </div>
+        )
+    }
+}
+
+Task.PropTypes = {
+    data: PropTypes.shape({
+        id: PropTypes.number,
+        title: PropTypes.string,
+    }),
+    onUpdate: PropTypes.func,
+    onRemove: PropTypes.func,
+}.isRequiered;
