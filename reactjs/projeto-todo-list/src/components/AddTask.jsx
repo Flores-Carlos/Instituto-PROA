fa553b48176df89be90c9300140326db2784e5ad
Carlos Flores <carlos.freitas15@outlook.com>
2022-05-27 20:31:08 -0300

    commit inicial do projeto

---
+++ reactjs/projeto-todo-list/src/components/AddTask.jsx
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import generators from '../lib/generators';
+
+export default class AddTask extends Component {
+    constructor() {
+        super();
+
+        this.initialState = {
+            id: 0,
+            title: '',
+            hasFinished: false,
+        };
+
+        this.state = this.initialState;
+
+        this.state = {
+            id: 0,
+            title: '',
+        };
+
+        this.handleInput = this.handleInput.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleInput(event) {
+        const { randomId } = generators;
+        this.setState({
+            id: randomId(9999999999999),
+            title: event.target.value,
+        });
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+        const { onCreate } = this.props;
+        const { id } = this.state;
+        if (id > 0) {
+            onCreate(this.state);
+            this.setState(this.initialState);
+        }
+    }
+
+    render() {
+        const { title } = this.state;
+
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <input type="text" value={title} onChange={this.handleInput}></input>
+                <button type='submit'>Adicionar Tarefa</button>
+            </form>
+        );
+    }
+}
+
+AddTask.PropTypes = {
+    onCreate: PropTypes.func,
+}.isRequiered;
