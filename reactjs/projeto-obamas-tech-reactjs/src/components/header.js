e8009110b4e29e25f345adf1a967427deca6a7fc
Carlos Flores <carlos.freitas15@outlook.com>
2022-05-27 21:55:14 -0300

    adicionar icone de lupa no botão de busca

--- reactjs/projeto-obamas-tech-reactjs/src/components/header.js
+++ reactjs/projeto-obamas-tech-reactjs/src/components/header.js
@@ -1,18 +1,17 @@
 import React, { Component } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { BiSearch } from 'react-icons/bi';
 
 class Header extends Component {
     render() {
         return (
             <header className="cabecalho">
                 <h1 className="logo">
-                    {/* <img src='../img/logo2.png' /> */}
                     <a href="#" title="Obama's Tech">Obama's Tech</a>
                 </h1>
                 <form action="" method="post">
                     <input type="text" name="busca" id="busca" placeholder="Faça um busca aqui!" />
                     <button href="#">
-                        <img src='../img/glass' className="fa-solid fa-magnifying-glass"></img>
+                        <BiSearch />
                     </button>
                 </form>
             </header>
